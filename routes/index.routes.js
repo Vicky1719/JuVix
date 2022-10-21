2d1fc67bf544b17edb4a388c2e653cf0ccfa3e93
Vicky1719 <112522032+Vicky1719@users.noreply.github.com>
2022-10-21 17:59:07 +0200

    index

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,7 +6,18 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+// rutas
+const productRoutes = require("./product.routes.js")
+router.use("/product", productRoutes)
 
+const profileRoutes = require("./profile.routes.js")
+router.use("/profile", profileRoutes)
+
+const supplierRoutes = require("./supplier.routes.js")
+router.use("/supplier", supplierRoutes)
+
+const authRoutes = require("./auth.routes.js")
+router.use("/auth", authRoutes)
 
 
 module.exports = router;
