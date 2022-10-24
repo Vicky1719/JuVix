8cfdf41933cbd1f973045ead5b68fed51ed23178
Juvixa A <juvixa.aloayza@gmail.com>
2022-10-24 13:58:58 +0200

    product.routes creando categorias

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,8 +1,48 @@
 const router = require("express").Router();
+const { get } = require("mongoose");
 const Product = require("../models/Product.model");
 const Supplier = require("../models/Supplier.model");
 
+// GET para agregar producto
 
+router.get("/create", async (req, res, next) => {
+  try {
+    const productList = await Product.find();
+    res.render("product/create.hbs", {
+      productList,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+//POST recibir información del nuevo producto
+router.post("/create", (req, res, next) => {
+  let productAdd = {
+    name: req.body.name,
+    description: req.body.description,
+    category: req.body.category,
+    supplier: req.body.supplier,
+    administrador: req.body.administrador,
+  };
+});
+
+//GET "/products" => ruta para que el usuario vea la lista de productos según categoría:maquillaje
+router.get("/:category", (req, res, next) => {
+
+    let{category} = req.params
+
+    Product.findbyId(category)
+    .populate("maquillaje")
+    .then((response) => {
+        res.render("product/list.hbs", {
+            productCategory: response
+        })
+    })
+    .catch((error) => {
+        next(error)
+    })
+
+})
 
 
 module.exports = router;
--- routes/supplier.routes.js
+++ routes/supplier.routes.js
@@ -1,6 +1,25 @@
 const router = require("express").Router();
 const Supplier = require("../models/Supplier.model");
 
+//GET "/supplier/create" 
+router.get("/create", (req, res, next)=> {
+    res.render("supplier/create.hbs")
+})
+
+// POST "/supplier/create"
+router.post("/create", async (req, res, next) => {
+    const{ name, location} = req.body
+try{
+    await Supplier.create({
+        name,
+        location
+    })
+
+    res.redirect("/supplier")
+} catch(error){
+    next (error)
+}
+})
 
 
 
