7984715b4b104895209716546589a57de76cbe85
Vicky1719 <112522032+Vicky1719@users.noreply.github.com>
2022-10-24 18:38:56 +0200

    arreglado

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,8 +1,29 @@
 const router = require("express").Router();
+const { get } = require("mongoose");
 const Product = require("../models/Product.model");
 const Supplier = require("../models/Supplier.model");
 
+// GET para agregar producto
 
-
+router.get("/create", async (req, res, next) => {
+  try {
+    const productList = await Product.find();
+    res.render("product/create.hbs", {
+      productList,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+//POST recibir información del nuevo producto
+router.post("/create", (req, res, next) => {
+  let productAdd = {
+    name: req.body.name,
+    description: req.body.description,
+    category: req.body.category,
+    supplier: req.body.supplier,
+    administrador: req.body.administrador,
+  };
+});
 
 module.exports = router;
